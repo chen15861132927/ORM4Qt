d2f22be98b64c54b7d8d7450397188ba918cec71
warner chen <1045005960@qq.com>
2023-05-15 22:53:25 +0800

    add create model table

--- test/ORM_MainTestQtConsole/main.cpp
+++ test/ORM_MainTestQtConsole/main.cpp
@@ -4,23 +4,29 @@
 #if defined(_MSC_VER) && (_MSC_VER >= 1600)    
 # pragma execution_character_set("utf-8")    
 #endif
-int main(int argc, char *argv[])
+#include "../ORM_Model/ORM_Model.h"
+int main(int argc, char* argv[])
 {
-    QCoreApplication a(argc, argv);
-    auto db = ORMDatabase::addORMDatabase("QMYSQL");
-    db.setUserName("root");
-    db.setHostName("localhost");
-    db.setPassword("123456");
-    //Test open
-    if (!db.open())
-    {
-        QString temp = db.lastError().text();
-        qDebug() << "open fail" << "connect to mysql error" << temp;
-        return -1;
-    }
-    else
-    {
-        auto res=db.createDatabase("Test_ORMDatabase");
-    }
-    return a.exec();
+	QCoreApplication a(argc, argv);
+	auto db = ORMDatabase::addORMDatabase("QMYSQL");
+	db.setUserName("root");
+	db.setHostName("localhost");
+	db.setPassword("123456");
+	//Test open
+	if (!db.open())
+	{
+		QString temp = db.lastError().text();
+		qDebug() << "open fail" << "connect to mysql error" << temp;
+		return -1;
+	}
+	else
+	{
+		auto res = db.createDatabase("Test_ORMDatabase");
+		ORM_Model model;
+
+		res = model.createTable();
+		qDebug() << "createTable:" << res;
+
+	}
+	return a.exec();
 }
